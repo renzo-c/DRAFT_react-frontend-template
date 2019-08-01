88f9a8e70cdafc2c1145bf467c1bcc5f7680bcaf
Renzo <rcnavarrop@gmail.com>
2019-08-01 10:33:48 -0500

    removing bable packets in dependencies to devDependencies

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-const title = "This is a basic react set up";
+const title = "This is a basic react set up asfas";
 
 ReactDOM.render(<App title={title} />, document.getElementById("app"));
