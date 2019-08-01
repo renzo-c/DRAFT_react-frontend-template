ea14b88f0ed7c3c014bd36d0ec5262379c3b5ca3
Renzo <rcnavarrop@gmail.com>
2019-08-01 13:24:43 -0500

    commenting proptype errors to generate build

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-const title = 'This is a basic react set up asfas';
+const title = 'This is a basic react set up for frontend proyects';
 
 ReactDOM.render(<App title={title} />, document.getElementById('app'));
