c721908f063e270d7492f4fe02e075dcfb391bf3
Renzo <rcnavarrop@gmail.com>
2019-08-01 13:56:52 -0500

    adding express server to deploy de project in heroku

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/dist/index.html"));
-  });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
-app.listen(PORT)
+app.listen(PORT);
 console.log(`Server listening on port ${PORT}`);
