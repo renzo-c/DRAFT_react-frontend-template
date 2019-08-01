18358f5f4e7a4115ae9a990eb323a5edc7922595
Renzo <rcnavarrop@gmail.com>
2019-08-01 14:22:45 -0500

    fixing typo in title message

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-const title = 'This is a basic react set up for frontend proyects';
+const title = 'This is a basic React.JS set up for frontend projects';
 
 ReactDOM.render(<App title={title} />, document.getElementById('app'));
